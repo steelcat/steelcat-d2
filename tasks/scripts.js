29c6d4e0fb7f9e37d12d45b4491ddc0319569ca8
Steelcat <me@steelcat.org>
2015-02-08 11:06:01 +0300

    Дошел до футера, подправил отслеживание файлов Gulp

--- tasks/scripts.js
+++ tasks/scripts.js
@@ -2,8 +2,8 @@ var gulp = require('gulp'),
 	$ = require('gulp-load-plugins')(); // автоматическая загрузка плагинов gulp
 
 gulp.task('scripts', function() {
-	return gulp.src('src/scripts/app.js')
-		.pipe($.browserify({debug: true})) // объединяем файлы с помощью browserify
+	return gulp.src('app/js/main.js')
+		//.pipe($.browserify({debug: true})) // объединяем файлы с помощью browserify
 		//.pipe($.uglify()) // минификация
 		.pipe(gulp.dest('public/js/')) // путь до сформированного файла
 		.pipe($.connect.reload()); // перезагружаем сервер
