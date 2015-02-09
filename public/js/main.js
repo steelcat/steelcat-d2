1cc7124ae8437d9fd338494a74da036e34611b92
Steelcat <me@steelcat.org>
2015-02-09 20:38:36 +0300

    Закончил задание легкого уровня

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,76 @@
-window.onload = (function () {
-	console.log('starting js');
-})();
+$(document).ready(function(){
+	$('.button-up').hide();
+
+	$(function () {
+		$(window).scroll(function () {
+			if ($(this).scrollTop() > 100) {
+				$('.button-up').fadeIn();
+			} else {
+				$('.button-up').fadeOut();
+			}
+		});
+
+		// scroll body to 0px on click
+		$('.button-up').click(function () {
+			$('body,html').animate({
+				scrollTop: 0
+			}, 800);
+			return false;
+		});
+	});
+
+	$(function(){
+
+		$('.sidebar-menu__item-link--trigger').on('click', function(e){
+			e.preventDefault();
+
+			var
+				$this = $(this),
+				menu = $this.closest('.sidebar-menu'),
+				item = $this.closest('.sidebar-menu__item'),
+				items = menu.find('.sidebar-menu__item'),
+				content = item.find('.sidebar-menu__inner-list'),
+				duration = 300;
+
+			console.log(item, items, menu);
+
+			if (!item.hasClass('active')) {
+				items.removeClass('active');
+				item.addClass('active');
+				items.find('.sidebar-menu__inner-list').slideUp(duration);
+				content.stop(true, true).slideDown(duration);
+
+			} else {
+				item.removeClass('active');
+				content.stop(true, true).slideUp(duration);
+			}
+		});
+	}());
+
+	$(document).ready(function(){
+
+		if ($('.content__options-select').length) {
+			$('.content__options-select').simpleSelect();
+		}
+
+		$('.selectbox__dropdown').jScrollPane();
+
+
+	});
+
+	$('.content__options-select').on('change', function(){
+		var
+			$this = $(this),
+			list = $('.product-list'),
+			view = $this.val();
+
+		if (view === 'cells') {
+			list.addClass('product-list--cells');
+		}
+
+		if (view === 'lines') {
+			list.removeClass('product-list--cells');
+		}
+
+	});
+});
--- tasks/styles.js
+++ tasks/styles.js
@@ -6,6 +6,6 @@ gulp.task('styles', function () {
 		.pipe($.sass({errLogToConsole: true})) // компиляция scss файлов в css
 		.pipe($.autoprefixer({ browsers: ['last 2 version'] })) // добавляем префиксы
 		//.pipe($.csso()) // минификация
-		.pipe(gulp.dest('public/styles')) // путь, куда ложим преобразованные стили
+		.pipe(gulp.dest('public/styles')) // путь, куда копируем преобразованные стили
 		.pipe($.connect.reload()); // перезагрузка сервера
 });
